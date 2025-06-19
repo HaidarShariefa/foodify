2bfa23d68e27b2c20f0b6d694bf0da437c38108b
Haidar <haidarshariefa33451@gmail.com>
2025-06-19 14:48:54 +0300

    outsource the routing logic to a router.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -1,48 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import AdminLayout from "./layouts/AdminLayout";
-import UserLayout from "./layouts/UserLayout";
+import { RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
+import router from "./router";
 import store from "./store/store";
 import "./index.css";
-
-// Admin pages
-import Dashboard from "./pages/admin/Dashboard";
-import Categories from "./pages/admin/Categories";
-import Items from "./pages/admin/Items";
-import Addons from "./pages/admin/Addons";
-import FullMenu from "./pages/admin/FullMenu";
-
-// User pages
-// import Home from "./pages/user/Home";
-// import Menu from "./pages/user/Menu";
-// import Cart from "./pages/user/Cart";
-// import Checkout from "./pages/user/Checkout";
-
-const router = createBrowserRouter([
-  {
-    path: "/admin",
-    element: <AdminLayout />,
-    children: [
-      { path: "dashboard", element: <Dashboard /> },
-      { path: "categories", element: <Categories /> },
-      { path: "items", element: <Items /> },
-      { path: "addons", element: <Addons /> },
-      { path: "menu", element: <FullMenu /> },
-    ],
-  },
-  // {
-  //   path: "/",
-  //   element: <UserLayout />,
-  //   children: [
-  //     { index: true, element: <Home /> },
-  //     { path: "menu", element: <Menu /> },
-  //     { path: "cart", element: <Cart /> },
-  //     { path: "checkout", element: <Checkout /> },
-  //   ],
-  // },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
---
+++ src/router.jsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+import AdminLayout from "./layouts/AdminLayout";
+import Categories from "./pages/admin/Categories";
+import Items from "./pages/admin/Items";
+import Addons from "./pages/admin/Addons";
+import FullMenu from "./pages/admin/FullMenu";
+
+const router = createBrowserRouter([
+  {
+    path: "/admin",
+    element: <AdminLayout />,
+    children: [
+      { path: "categories", element: <Categories /> },
+      { path: "items", element: <Items /> },
+      { path: "addons", element: <Addons /> },
+      { path: "menu", element: <FullMenu /> },
+    ],
+  },
+]);
+
+export default router;
